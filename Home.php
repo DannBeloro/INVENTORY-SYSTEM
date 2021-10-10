0727507ce7f46f24a079bc9b3611986eb14c4723
Marlon I C. Blando <91298997+Siobe17@users.noreply.github.com>
2021-10-10 21:25:50 +0800

    Create Home.php

---
+++ Home.php
@@ -0,0 +1,15 @@
+<?php
+
+$user = "root";
+&password = "";
+&databse = "books";
+
+$db = new mysqli ('localhost', $user ,$password, $database;
+                  
+                  
+ if ($db == false ){
+   echo "not connected";
+ }else{
+   echo : "wala pa katuloy";
+ }
+     
