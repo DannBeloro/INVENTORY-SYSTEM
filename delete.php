03cd10208b3166ab8a5c5f47d4f1e9dcd3e6efc1
Marlon I C. Blando <91298997+Siobe17@users.noreply.github.com>
2021-11-03 20:35:46 +0800

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,15 @@
+ <?php
+	include 'dbcon.php';
+  if(isset($_GET['deleteid'])){
+       $id=$_GET['deleteid'];
+
+  $sql = "DELETE FROM staffs WHERE id=$id";
+  $result=mysqli_query($con,$sql);
+  if($result){
+    // echo "Deleted Successfuly";
+    header('location:staffdisplay.php');
+}else{
+ die (mysqli_error($coon));
+}
+  }
+   ?>
